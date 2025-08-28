88c9d091dee5e125b364a0826c44300f27d415bd
modasserhossain-dsi <modasser.hossain@dsinnovators.com>
2025-08-28 22:56:16 +0600

    chor: dsa package created

--- java/main/com/skytech/leetcode/soln/longestCommonPrefix/LongestCommonPrefixSoln.java
+++ java/main/com/skytech/leetcode/soln/longestCommonPrefix/LongestCommonPrefixSoln.java
@@ -55,12 +55,15 @@ public class LongestCommonPrefixSoln {
 
     public String longestCommonPrefix(String[] strs) {
 
+        // If the input array is null or empty → no prefix possible → return ""
         if (strs == null || strs.length == 0) return "";
 
         String prefix = strs[0]; // take first string as prefix
 
         for (int i = 1; i < strs.length; i++) {
+            // checks if the current string starts with the prefix
             while (!strs[i].startsWith(prefix)) {
+                // shrink the prefix by removing the last character
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if (prefix.isEmpty()) return "";
             }
